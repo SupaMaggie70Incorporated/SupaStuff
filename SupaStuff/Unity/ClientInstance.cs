baf4a46e902632bb44f50ba7298e2a72af33f612
SupaMaggie70 <[magnus.larsson.mn@gmail.com]>
2022-10-18 22:13:52 -0500

    Added ServerInstance and ClientInstance

---
+++ SupaStuff/Unity/ClientInstance.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+using UnityEngine;
+
+
+using SupaStuff.Net.ClientSide;
+using SupaStuff.Net.ServerSide;
+using SupaStuff.Net.Packets;
+
+
+namespace SupaStuff.Unity
+{
+    [UnitySpecific]
+    public class ClientInstance<T> : MonoBehaviour
+    {
+        public static ClientInstance<T> Instance = null;
+        public Client client = null;
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(this);
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+        private void FixedUpdate()
+        {
+            if (client != null && client.IsActive) client.Update();
+        }
+        public void Initialize(IPAddress address, int port, byte[] password)
+        {
+            Debug.Log("Started");
+            client = new Client(address, port, password);
+            client.packetStream.OnDisconnected += () => { Debug.Log("Disconnected"); };
+            Debug.Log("Finished");
+        }
+        public void InitializeLocal(LocalClientConnection<T> connection)
+        {
+            Debug.Log("Started local client");
+            client = connection.client;
+            Debug.Log("Finished");
+        }
+    }
+}
---
+++ SupaStuff/Unity/ServerInstance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+using UnityEngine;
+
+using SupaStuff.Net.ServerSide;
+
+
+namespace SupaStuff.Unity
+{
+    [UnitySpecific]
+    public class ServerInstance<T> : MonoBehaviour
+    {
+        public static ServerInstance<T> Instance;
+        public Server<T> server;
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(this);
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+        private void FixedUpdate()
+        {
+            if (server != null && server.isActive) server.Update();
+        }
+        public void InitializeDedicated(int maxConnections, int port, byte[] password)
+        {
+            server = new Server<T>(maxConnections, port, password);
+            server.localConnection.Dispose();
+            server.StartListening();
+        }
+        public void InitializeLocal(int maxConnections, int port, byte[] password)
+        {
+            server = new Server<T>(maxConnections, port, password);
+            server.StartListening();
+            ClientInstance<T>.Instance.InitializeLocal(server.localConnection);
+        }
+    }
+}
